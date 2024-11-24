a38fba344f644f5012bb992e6e84d970f1b2b933
Davide Ginelli <d.ginelli@reply.it>
2024-11-24 18:09:56 +0100

    Initial Commit

---
+++ bitirise-test/bitirise_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitirise_testApp.swift
+//  bitirise-test
+//
+//  Created by Davide Ginelli on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct bitirise_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
